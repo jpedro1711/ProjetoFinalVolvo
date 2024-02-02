1c05c50c4a87683bf3c3a3b21510414db5f36e74
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2024-02-02 17:07:49 -0300

    remove endpoints ViewController - vendas

--- ConcessionariaAPI/Controllers/ViewControllers/VendasController.cs
+++ ConcessionariaAPI/Controllers/ViewControllers/VendasController.cs
@@ -1,5 +1,4 @@
 ﻿using ConcessionariaAPI.Models;
-using ConcessionariaAPI.Models.ViewModels;
 using ConcessionariaAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -41,61 +40,6 @@ namespace ConcessionariaAPI.Controllers.ViewControllers
 
         }
 
-        [HttpGet("ConfirmDelete/{id}")]
-        public async Task<IActionResult> ConfirmDelete(int id)
-        {
-            try
-            {
-                var venda = await _service.GetById(id);
-                return View(venda);
-            }
-            catch (Exception e)
-            {
-                return RedirectToAction("Index");
-            }
-
-        }
-
-        [HttpGet("Delete/{VendaId}")]
-        public async Task<IActionResult> Delete(int VendaId)
-        {
-            await _service.Delete(VendaId);
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet("Edit/{id}")]
-        public async Task<IActionResult> Edit(int id)
-        {
-            try
-            {
-                var venda = await _service.GetById(id);
-                return View(venda);
-            }
-            catch (Exception e)
-            {
-                return RedirectToAction("Index");
-            }
-
-        }
-
-        [HttpGet("Create")]
-        public async Task<IActionResult> Create()
-        {
-            var cars = await _veiculoService.GetAll();
-            var sellers = await _vendedorService.GetAll();
-            var viewModel = new VendaFormViewModel { Veiculos = cars, Vendedores = sellers };
-            return View(viewModel);
-        }
-
-        [HttpPost("Create")]
-        public async Task<IActionResult> Create([FromForm] Venda venda)
-        {
-            var veiculo = await _veiculoService.GetById(venda.VeiculoId);
-            venda.Veiculo.NumeroChassi = veiculo.NumeroChassi;
-            venda.Veiculo.VersaoSistema = veiculo.VersaoSistema;
-
-            await _service.Create(venda);
-            return RedirectToAction("Index");
-        }
+        
     }
 }
