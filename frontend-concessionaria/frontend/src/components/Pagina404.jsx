b59a1ee256b41b98343c445cb0431aff52b2f561
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2024-02-06 08:35:11 -0300

    ajustes pagina 404

--- frontend-concessionaria/frontend/src/components/Pagina404.jsx
+++ frontend-concessionaria/frontend/src/components/Pagina404.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Pagina404 = () => {
-  return <div>404 - Erro</div>;
+  const appStyle = {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
+  return (
+    <div className="p-5 mt-5" style={appStyle}>
+      <strong>404 - Erro</strong>
+      <br />
+      <a href="/" className="btn btn-secondary">
+        Voltar ao começo
+      </a>
+    </div>
+  );
 };
 
 export default Pagina404;
